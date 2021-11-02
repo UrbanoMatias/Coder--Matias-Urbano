86975b565eb5957a22251e00deff21a0c33a7cf3
Matias Urbano <matiiurbano07@gmail.com>
2021-11-02 00:20:45 -0300

    navbar

---
+++ src/components/navBar.js
@@ -0,0 +1,14 @@
+export const NavBar = () => {
+    return (
+        <header className="header">
+            <h1>My Phone</h1>
+            <nav>
+                <ul>
+                    <li>Contacto</li>
+                    <li>Producto</li>
+                    <li>Carrito</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
